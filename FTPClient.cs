ba160b880d97354c898518d77ed128cb2f1b6904
Matyasmester <matyas.zoboki@gmail.com>
2024-06-21 15:44:45 +0200

    Add support for retrieving files

--- FTPClient.cs
+++ FTPClient.cs
@@ -15,16 +15,23 @@ namespace FTPClient
     public class FTPClient : IDisposable
     {
         private IPEndPoint endPoint;
+
         private TcpClient client = new TcpClient();
+        private TcpClient dataClient = new TcpClient();
 
         private StreamReader reader;
         private StreamWriter writer;
 
         private NetworkStream stream;
+        private NetworkStream dataStream;
+
+        private static readonly string SharingFolderName = "ToShare";
+        private readonly string SharingFolderPath = Path.Combine(Directory.GetCurrentDirectory(), SharingFolderName);
 
         public FTPClient()
         {
-            
+            if(!Directory.Exists(SharingFolderPath)) Directory.CreateDirectory(SharingFolderPath);
+            Directory.SetCurrentDirectory(SharingFolderPath);
         }
 
         public string Connect(IPAddress IP, int port)
@@ -42,17 +49,26 @@ namespace FTPClient
 
         public async Task<string> SendCommand(string command)
         {
+            string[] cmdSplit = command.Split(' ');
+            string cmd = cmdSplit[0].ToLower();
+
             writer.WriteAsync(command + "\r\n").Wait();
             writer.FlushAsync().Wait();
 
+            if (cmd.Equals("GET".ToLower()) || cmd.Equals("RETR".ToLower()))
+            {
+                MakeDataConnection();
+                await GetCopyOfFile(cmdSplit[1]);
+            }
+
             char[] buffer = new char[1024];
-            
+
             while(!stream.DataAvailable)
             {
                 await Task.Delay(40);
             }
 
-            while(stream.DataAvailable) await reader.ReadAsync(buffer, 0, buffer.Length - 1);
+            while(stream.DataAvailable) await reader.ReadAsync(buffer, 0, buffer.Length);
 
             return new string(buffer);
         }
@@ -60,8 +76,50 @@ namespace FTPClient
         public void Dispose()
         {
             client.Dispose();
+            dataClient.Dispose();
+
             reader.Dispose();
             writer.Dispose();
+
+            stream.Dispose();
+            dataStream.Dispose();
+        }
+
+        private void MakeDataConnection()
+        {
+            if(dataClient.Connected) return;
+
+            dataClient.Connect(endPoint.Address, endPoint.Port + 1);
+            dataStream = dataClient.GetStream();
+        }
+
+        private async Task GetCopyOfFile(string name)
+        {
+            int bufferSize = 1024;
+
+            byte[] fileSizeBytes = new byte[4];
+            dataStream.ReadAsync(fileSizeBytes, 0, 4).Wait();
+
+            int fileSize = BitConverter.ToInt32(fileSizeBytes, 0);
+
+            int bytesLeft = fileSize;
+            byte[] fileContent = new byte[fileSize];
+
+            int bytesRead = 0;
+
+            while(bytesLeft > 0)
+            {
+                int currentSize = Math.Min(bytesLeft, bufferSize);
+
+                if(dataClient.Available < currentSize) currentSize = dataClient.Available;
+
+                await dataStream.ReadAsync(fileContent, bytesRead, currentSize);
+
+                bytesRead += currentSize;
+                bytesLeft -= currentSize;
+            }
+
+            File.WriteAllBytes(name, fileContent);
         }
     }
 }
